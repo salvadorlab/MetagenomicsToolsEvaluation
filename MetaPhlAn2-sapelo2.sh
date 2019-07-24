b9a8f6560f42af4e0590a6849970bee0d1ef6c3f
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-07-24 16:37:22 -0700

    Add files via upload

---
+++ MetaPhlAn2-sapelo2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -q batch                                                            
+#PBS -N MetaPhlAn2-test                                            
+#PBS -l nodes=1:ppn=2 -l mem=20gb                                        
+#PBS -l walltime=10:00:00                                                
+#PBS -M rx32940@uga.edu                                                  
+#PBS -m abe                                                              
+#PBS -o /scratch/rx32940/Metagenomic_taxon_profile                       
+#PBS -e /scratch/rx32940/Metagenomic_taxon_profile                         
+#PBS -j oe     
+
+
+cd $PBS_O_WORKDIR
+
+module load MetaPhlAn2/2.7.8-foss-2016b-Python-2.7.14
+
+metaphlan2.py /Users/MACBOOK/Downloads/R22.K_1_kneaddata_paired_1.fastq --input_type fastq > /Users/MACBOOK/Downloads/R22.K_metaPhlAn2.txt
