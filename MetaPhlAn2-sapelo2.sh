aacc00daaaecbf9bd86521c09576021a337ec971
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-07-25 16:56:33 -0400

    test

--- MetaPhlAn2-sapelo2.sh
+++ MetaPhlAn2-sapelo2.sh
@@ -15,5 +15,7 @@ cd $PBS_O_WORKDIR
 module load MetaPhlAn2/2.7.8-foss-2016b-Python-2.7.14
 module load Bowtie2/2.3.4.1-foss-2016b
 
-path = '/scratch/rx32940/Metagenomic_taxon_profile/Data/01.Data/hostclean/'
-metaphlan2.py $path/R22.K/R22.K_1_kneaddata_paired_1.fastq $path/R22.K/R22.K_1_kneaddata_paired_2.fastq --bowtie2out R22.K.bowtie2out.bz2 --input_type fastq --bowtie2db METAPHLAN_BOWTIE2_DB > /scratch/rx32940/Metagenomic_taxon_profile/R22.K_metaPhlAn2.txt
+path='/scratch/rx32940/Metagenomic_taxon_profile/Data/01.Data/hostclean/'
+metaphlan2.py $path/R22.K/R22.K_1_kneaddata_paired_1.fastq $path/R22.K/R22.K_1_kneaddata_paired_2.fastq --bowtie2out R22.K.bowtie2out.bz2 --input_type fastq > /scratch/rx32940/Metagenomic_taxon_profile/R22.K_metaPhlAn2.txt
+
+# --bowtie2db METAPHLAN_BOWTIE2_DB
