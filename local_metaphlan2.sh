e7053dd808911e63b9962aa8e6e2feba5b237f1c
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-07-29 16:14:56 -0400

    local try raw

--- local_metaphlan2.sh
+++ local_metaphlan2.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-metaphlan2.py $path/Data/01.Data/hostclean/R22.K/R22.K_1_kneaddata_paired_1.fastq $path/Data/01.Data/hostclean/R22.K/R22.K_1_kneaddata_paired_2.fastq --input_type fastq --bowtie2out $path/R22.K.bowtie2out.bz2 > $path/R22.K_metaPhlAn2.txt
+
+source activate metaphlan2
+
+path='/Users/rx32940/Desktop'
+metaphlan2.py $path/01.Data/rawdata/R22.K/R22.K_1.fq.gz,$path/01.Data/rawdata/R22.K/R22.K_1.fq.gz --input_type fastq --bowtie2out $path/01.Data/R22.K.bowtie2out.bz2 > $path/01.Data/R22.K_metaPhlAn2.txt
+#metaphlan2.py metagenome.fastq --bowtie2out metagenome.bowtie2.bz2 --nproc 5 --input_type fastq > profiled_metagenome.txt
+#
