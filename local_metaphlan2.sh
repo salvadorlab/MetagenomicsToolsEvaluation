20dbdd5cc3da446c536af4908fb88b1590f5560c
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-07-29 17:11:34 -0400

    try kraken

--- local_metaphlan2.sh
+++ local_metaphlan2.sh
@@ -4,6 +4,7 @@
 source activate metaphlan2
 
 path='/Users/rx32940/Desktop'
-metaphlan2.py $path/01.Data/rawdata/R22.K/R22.K_1.fq.gz,$path/01.Data/rawdata/R22.K/R22.K_1.fq.gz --input_type fastq --bowtie2out $path/01.Data/R22.K.bowtie2out.bz2 > $path/01.Data/R22.K_metaPhlAn2.txt
-#metaphlan2.py metagenome.fastq --bowtie2out metagenome.bowtie2.bz2 --nproc 5 --input_type fastq > profiled_metagenome.txt
-#
+metaphlan2.py $path/01.Data/hostclean/R22.L/R22.L_1_kneaddata_paired_1.fastq,$path/01.Data/hostclean/R22.L/R22.L_1_kneaddata_paired_2.fastq --input_type fastq --bowtie2out $path/01.Data/R22.L.bowtie2out.bz2 > $path/01.Data/R22.L_metaPhlAn2.txt
+# metaphlan2.py metagenome.fastq --bowtie2out metagenome.bowtie2.bz2 --nproc 5 --input_type fastq > profiled_metagenome.txt
+
+#metaphlan2.py $path/01.Data/R22.K.bowtie2out.bz2 --input_type bowtie2out > $path/01.Data/profiled_metagenome.txt
