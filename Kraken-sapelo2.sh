e7e2b00418a6c47af70edc49b4e20ff8446d3137
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-08-01 16:43:34 -0400

    c

--- Kraken-sapelo2.sh
+++ Kraken-sapelo2.sh
@@ -13,13 +13,13 @@ module load Kraken2/2.0.7-beta-foss-2018a-Perl-5.26.1
 module load KronaTools/2.7
 
 
-path='/scratch/rx32940/Metagenomic_taxon_profile'
 
+path='/scratch/rx32940/Metagenomic_taxon_profile'
 for dir in $path/Data/01.Data/hostclean/*; do
     echo $dir
     sample=$(echo $dir | awk -F'[/]' '{print $8}')
     echo "$path/Data/01.Data/hostclean/$sample/${sample}_1_kneaddata_paired_1.fastq"
-    kraken2 --use-names --db $path/kraken/minikraken2_v1_8GB_201904_UPDATE --threads 4 --report $sample.report.txt --paired $path/Data/01.Data/hostclean/$sample/${sample}_1_kneaddata_paired_1.fastq $path/Data/01.Data/hostclean/$sample/${sample}_1_kneaddata_paired_2.fastq > $path/kraken_output/$sample.txt
+    kraken2 --use-names --db $path/kraken/minikraken2_v1_8GB_201904_UPDATE --threads 4 --report $path/kraken_output/$sample.report.txt --paired $path/Data/01.Data/hostclean/$sample/${sample}_1_kneaddata_paired_1.fastq $path/Data/01.Data/hostclean/$sample/${sample}_1_kneaddata_paired_2.fastq > $path/kraken_output/$sample.txt
     cat $path/kraken_output/$sample.txt | cut -f 2,3 > $path/kraken_output/$sample.krona
     ktImportTaxonomy evolved-6.kraken.krona
 done
