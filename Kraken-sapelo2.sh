80c39f9d26e193334e947d6d189fbbf0e071e27a
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-08-02 17:14:50 -0400

    com

--- Kraken-sapelo2.sh
+++ Kraken-sapelo2.sh
@@ -10,12 +10,13 @@
 #PBS -j oe   
 
 module load Kraken2/2.0.7-beta-foss-2018a-Perl-5.26.1
-module load Bracken/2.2-foss-2016b
+# Bracken not usable on sapelo2, will run it locally, can't find "/usr/local/apps/eb/Bracken/2.2-foss-2016b/bin/src/est_abundance.py"
+# krona not usable either, can't update taxonomy labels, no access for the folder
 
 path='/scratch/rx32940/Metagenomic_taxon_profile'
 for dir in $path/Data/01.Data/hostclean/*; do
     sample=$(echo $dir | awk -F'[/]' '{print $8}')
     kraken2 --use-names --db $path/kraken/minikraken2_v1_8GB_201904_UPDATE --threads 4 --use-mpa-style --report $path/kraken_output/$sample.report.txt --paired $path/Data/01.Data/hostclean/$sample/${sample}_1_kneaddata_paired_1.fastq $path/Data/01.Data/hostclean/$sample/${sample}_1_kneaddata_paired_2.fastq > $path/kraken_output/$sample.txt
-    time bracken -d $path/kraken/minikraken2_v1_8GB_201904_UPDATE -i $path/kraken_output/$sample.txt -l S -o $path/kraken_output/$sample.txt.bracken
+    # time bracken -d $path/kraken/minikraken2_v1_8GB_201904_UPDATE -i $path/kraken_output/$sample.txt -l S -o $path/kraken_output/$sample.txt.bracken
     cat $path/kraken_output/$sample.txt | cut -f 2,3 > $path/kraken_output/$sample.krona
 done
