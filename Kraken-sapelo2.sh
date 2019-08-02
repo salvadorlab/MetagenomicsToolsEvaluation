41d977121451b6c3014df1ded2109f97b5ccc316
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-08-02 13:07:52 -0400

    commit

--- Kraken-sapelo2.sh
+++ Kraken-sapelo2.sh
@@ -10,12 +10,12 @@
 #PBS -j oe   
 
 module load Kraken2/2.0.7-beta-foss-2018a-Perl-5.26.1
+module load Bracken/2.2-foss-2016b-Python-2.7.14
 
 path='/scratch/rx32940/Metagenomic_taxon_profile'
 for dir in $path/Data/01.Data/hostclean/*; do
-    echo $dir
     sample=$(echo $dir | awk -F'[/]' '{print $8}')
-    echo "$path/Data/01.Data/hostclean/$sample/${sample}_1_kneaddata_paired_1.fastq"
     kraken2 --use-names --db $path/kraken/minikraken2_v1_8GB_201904_UPDATE --threads 4 --use-mpa-style --report $path/kraken_output/$sample.report.txt --paired $path/Data/01.Data/hostclean/$sample/${sample}_1_kneaddata_paired_1.fastq $path/Data/01.Data/hostclean/$sample/${sample}_1_kneaddata_paired_2.fastq > $path/kraken_output/$sample.txt
+    bracken -d $path/kraken/minikraken2_v1_8GB_201904_UPDATE -i $path/kraken_output/$sample.txt -l S -o $path/kraken_output/$sample.txt.bracken
     cat $path/kraken_output/$sample.txt | cut -f 2,3 > $path/kraken_output/$sample.krona
 done
