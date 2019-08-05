c59b916729e56a5c73b693345fd85bf87b226fd9
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-08-05 13:01:58 -0400

    new software

---
+++ MetaBAT_sapelo2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -q batch                                                            
+#PBS -N Kraken-test                                            
+#PBS -l nodes=1:ppn=2 -l mem=10gb                                        
+#PBS -l walltime=5:00:00                                                
+#PBS -M rx32940@uga.edu                                                  
+#PBS -m abe                                                              
+#PBS -o /scratch/rx32940/Metagenomic_taxon_profile                       
+#PBS -e /scratch/rx32940/Metagenomic_taxon_profile                         
+#PBS -j oe   
+
+# assembly based binning 
+module load MetaBAT/2.12.1-foss-2018a-linux_x86_64
