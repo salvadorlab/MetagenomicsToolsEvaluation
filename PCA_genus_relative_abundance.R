c38e3d92f0589a1bfe2b6df75a7a1f256ca2afed
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-08-15 12:29:27 -0400

    commit

--- PCA_genus_relative_abundance.R
+++ PCA_genus_relative_abundance.R
@@ -1,3 +1,5 @@
-
+library(xlsx)
 setwd("/Users/rx32940/Dropbox/5. Rachel's projects/Metagenomic_Analysis")
+
+data <- read.xlsx2("relative_abundance_genus.xlsx",1,header=T,rownames=T,colClasses = NA)
 prcomp()
