81e174444c273aed11b4472a31800a005f1f727c
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-08-19 14:22:23 -0400

    commit

--- PCA_genus_relative_abundance.R
+++ PCA_genus_relative_abundance.R
@@ -1,5 +1,25 @@
 library(xlsx)
+library(devtools)
+library(factoextra) #for pca visualization
+
+# this pca analysis is done with relative abundance of bacteria and viruses composition in the samples
+# the report from the company is based only on bacterial pyhlum relative abundance (viruses do not classify by phylum)
+
+#tutorial for pca
+# http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/118-principal-component-analysis-in-r-prcomp-vs-princomp/#prcomp-and-princomp-functions
+
+
+
 setwd("/Users/rx32940/Dropbox/5. Rachel's projects/Metagenomic_Analysis")
 
-data <- read.xlsx2("relative_abundance_genus.xlsx",1,header=T,rownames=T,colClasses = NA)
-prcomp()
+data <- read.xlsx2("relative_abundance_genus.xlsx",1,header=T,colIndex = 1:13)
+#rownames(data) <- data[,1]
+data <- data[,-1]
+
+data2 <- data.frame(lapply(data, function(x) as.numeric(as.character(x))))
+data2<-t(data2)
+pca <- prcomp(data2,center=TRUE,scale. = TRUE)
+summary(pca)
+
+eigenvalues <- fviz_eig(pca)
+fviz_pca_ind(pca,col.var="cos2")
