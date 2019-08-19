5ae384e92b1ec4681cf34e17ebf02ead66f9cc8a
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-08-19 16:44:42 -0400

    commit

--- PCA_genus_relative_abundance.R
+++ PCA_genus_relative_abundance.R
@@ -13,13 +13,16 @@ library(factoextra) #for pca visualization
 setwd("/Users/rx32940/Dropbox/5. Rachel's projects/Metagenomic_Analysis")
 
 data_genus <- read.xlsx2("relative_abundance_genus.xlsx",1,header=T,colIndex = 1:13)
-#rownames(data) <- data[,1]
-data_genus <- data[,-1]
+rownames(data_genus) <- data_genus[,1]
+data_genus <- data_genus[,-1]
 
-data2_genus <- data.frame(lapply(data, function(x) as.numeric(as.character(x))))
+data2_genus <- data.frame(lapply(data_genus, function(x) as.numeric(as.character(x))))
 data2_genus <-t(data2_genus)
 pca_genus <- prcomp(data2_genus,center=TRUE,scale. = TRUE)
-summary(pca)
+summary(pca_genus)
 
 eigenvalues_genus <- fviz_eig(pca_genus)
-pca_genus_plot <- fviz_pca_ind(pca_genus,col.var="cos2")
+
+png("pca_genus_plot.png")
+fviz_pca_ind(pca_genus,col.var="cos2")
+dev.off()
