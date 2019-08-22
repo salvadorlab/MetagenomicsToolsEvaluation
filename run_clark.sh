6b170487826838b13b614e296e869279d8abc67b
Ruijie Xu <rx32940@sapelo2-sub1.ecompute>
2019-08-22 13:40:48 -0400

    commit

--- run_clark.sh
+++ run_clark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q highmem_q                                                            
 #PBS -N run_clark                                            
-#PBS -l nodes=1:ppn=2 -l mem=300gb                                        
+#PBS -l nodes=1:ppn=2 -l mem=200gb                                        
 #PBS -l walltime=100:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
@@ -26,4 +26,4 @@ $path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -P $path/CLARK/sample.L.txt $
 echo "classify_metagenome done"
 
 # databases of discriminative spaced 31-mers
-$path/CLARK/CLARKSCV1.2.6.1/buildSpacedDB.sh
+#$path/CLARK/CLARKSCV1.2.6.1/buildSpacedDB.sh
