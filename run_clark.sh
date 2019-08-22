5429833f8ac9bec5b1e9e0e70ba801d29df78ada
Ruijie Xu <rx32940@sapelo2-sub1.ecompute>
2019-08-22 12:53:57 -0400

    commit

--- run_clark.sh
+++ run_clark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q batch                                                            
 #PBS -N run_clark                                            
-#PBS -l nodes=1:ppn=4 -l mem=200gb                                        
+#PBS -l nodes=1:ppn=2 -l mem=100gb                                        
 #PBS -l walltime=100:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
