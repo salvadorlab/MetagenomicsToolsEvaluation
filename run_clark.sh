036ed163fd7a50d55a1a3532ba2bae9914ab3417
Ruijie Xu <rx32940@sapelo2-sub2.ecompute>
2019-08-25 21:45:53 -0400

    build spaced database

--- run_clark.sh
+++ run_clark.sh
@@ -14,16 +14,16 @@
 path="/scratch/rx32940"
 
 # set up the database
-$path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB bacteria viruses --species
-$path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB bacteria viruses --genus
+#$path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB bacteria viruses --species
+#$path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB bacteria viruses --genus
 
 echo "set target done"
 
 
 # database of discriminative 31-mers
-$path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -P $path/CLARK/sample.L.txt $path/CLARK/sample.R.txt -R /scratch/rx32940/CLARK/output/result
+#$path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -P $path/CLARK/sample.L.txt $path/CLARK/sample.R.txt -R /scratch/rx32940/CLARK/output/result
 
 echo "classify_metagenome done"
 
 # databases of discriminative spaced 31-mers
-#$path/CLARK/CLARKSCV1.2.6.1/buildSpacedDB.sh
+$path/CLARK/CLARKSCV1.2.6.1/buildSpacedDB.sh
