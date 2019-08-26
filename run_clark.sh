ea3a3a8bbd2a7c2afd8c046f8a9de9b3b59afd04
Ruijie Xu <rx32940@sapelo2-sub1.ecompute>
2019-08-26 11:15:11 -0400

    only build database test

--- run_clark.sh
+++ run_clark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -q highmem_q                                                            
 #PBS -N run_clark                                            
-#PBS -l nodes=1:ppn=2 -l mem=200gb                                        
-#PBS -l walltime=50:00:00                                                
+#PBS -l nodes=1:ppn=2 -l mem=400gb                                        
+#PBS -l walltime=100:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
 #PBS -o /scratch/rx32940/CLARK                       
@@ -14,14 +14,14 @@
 path="/scratch/rx32940"
 
 # set up the database
-$path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB bacteria viruses --species
-$path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB bacteria viruses --genus
+#$path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB bacteria viruses --species
+#$path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB bacteria viruses --genus
 
 echo "set target done"
 
 
 # database of discriminative 31-mers
-$path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -P $path/Metagenomic_taxon_profile/Data/01.Data/hostclean/R22.K/R22.K_1_kneaddata_paired_1.fastq $path/Metagenomic_taxon_profile/Data/01.Data/hostclean/R22.K/R22.K_1_kneaddata_paired_2.fastq -R /scratch/rx32940/CLARK/output/result
+#$path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -P $path/Metagenomic_taxon_profile/Data/01.Data/hostclean/R22.K/R22.K_1_kneaddata_paired_1.fastq $path/Metagenomic_taxon_profile/Data/01.Data/hostclean/R22.K/R22.K_1_kneaddata_paired_2.fastq -R /scratch/rx32940/CLARK/output/result
 
 echo "classify_metagenome done"
 
