a91b9619b730e40e4880a4399435e187d95b177f
rx32940 <rx32940@uga.edu>
2019-08-27 15:47:29 -0400

    test username

--- run_clark.sh
+++ run_clark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -q bahl_salv_q                                                           
-#PBS -N loop_base_clark                                            
-#PBS -l nodes=1:ppn=2 -l mem=100gb                                        
+#PBS -q batch                                                           
+#PBS -N abundance_samplers                                            
+#PBS -l nodes=1:ppn=2 -l mem=20gb                                        
 #PBS -l walltime=100:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
@@ -24,14 +24,18 @@ data_path="/scratch/rx32940/Metagenomic_taxon_profile/Data/01.Data/hostclean"
 # database of discriminative 31-mers
 # couldn't get a list of fastq files to run together, try to run individually now
 
-for file in $data_path/*; do
-    sample=$(basename "$file")
-    $path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -P $data_path/$sample/${sample}_1_kneaddata_paired_1.fastq $data_path/$sample/${sample}_1_kneaddata_paired_2.fastq -R $path/CLARK/output/$sample.txt
-done
+# for file in $data_path/*; do
+#     sample=$(basename "$file")
+#     $path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -P $data_path/$sample/${sample}_1_kneaddata_paired_1.fastq $data_path/$sample/${sample}_1_kneaddata_paired_2.fastq -R $path/CLARK/output/$sample.txt
+# done
+
+#$path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -P $path/Metagenomic_taxon_profile/Data/01.Data/hostclean/R22.K/R22.K_1_kneaddata_paired_1.fastq $path/Metagenomic_taxon_profile/Data/01.Data/hostclean/R22.K/R22.K_1_kneaddata_paired_2.fastq -R /scratch/rx32940/CLARK/output/result
+
 #echo "classify_metagenome done"
 
 # analyze result from regular clark
-#$path/CLARK/CLARKSCV1.2.6.1/estimate_abundance.sh -F /scratch/rx32940/CLARK/output/result.csv -D $path/CLARK/DB > /scratch/rx32940/CLARK/output/result_abundance.txt
+$path/CLARK/CLARKSCV1.2.6.1/estimate_abundance.sh -F /scratch/rx32940/CLARK/output/R22.L.txt.csv -D $path/CLARK/DB > /scratch/rx32940/CLARK/output/R22.L_abundance.txt
 
+$path/CLARK/CLARKSCV1.2.6.1/estimate_abundance.sh -F /scratch/rx32940/CLARK/output/R22.S.txt.csv -D $path/CLARK/DB > /scratch/rx32940/CLARK/output/R22.S_abundance.txt
 # databases of discriminative spaced 31-mers
 #$path/CLARK/CLARKSCV1.2.6.1/buildSpacedDB.sh
