58b25b7dbe262b0f94a54355930795e302b009e9
Rachel <rx32940@uga.edu>
2019-09-03 15:40:14 -0400

    run phylum for bacteria composition

--- run_clark.sh
+++ run_clark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -q batch                                                           
+#PBS -q bahl_salv_q                                                          
 #PBS -N abundance_samplers                                            
-#PBS -l nodes=1:ppn=2 -l mem=20gb                                        
+#PBS -l nodes=1:ppn=2 -l mem=100gb                                        
 #PBS -l walltime=100:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
@@ -15,7 +15,7 @@ path="/scratch/rx32940"
 data_path="/scratch/rx32940/Metagenomic_taxon_profile/Data/01.Data/hostclean"
 
 # set up the database
-#$path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB bacteria viruses --species
+$path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB bacteria viruses --phylum
 #$path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB bacteria viruses --genus
 
 #echo "set target done"
@@ -24,10 +24,10 @@ data_path="/scratch/rx32940/Metagenomic_taxon_profile/Data/01.Data/hostclean"
 # database of discriminative 31-mers
 # couldn't get a list of fastq files to run together, try to run individually now
 
-# for file in $data_path/*; do
-#     sample=$(basename "$file")
-#     $path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -P $data_path/$sample/${sample}_1_kneaddata_paired_1.fastq $data_path/$sample/${sample}_1_kneaddata_paired_2.fastq -R $path/CLARK/output/$sample.txt
-# done
+for file in $data_path/*; do
+     sample=$(basename "$file")
+     $path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -P $data_path/$sample/${sample}_1_kneaddata_paired_1.fastq $data_path/$sample/${sample}_1_kneaddata_paired_2.fastq -R $path/CLARK/output/bacteria_phylum/$sample.txt
+done
 
 #$path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -P $path/Metagenomic_taxon_profile/Data/01.Data/hostclean/R22.K/R22.K_1_kneaddata_paired_1.fastq $path/Metagenomic_taxon_profile/Data/01.Data/hostclean/R22.K/R22.K_1_kneaddata_paired_2.fastq -R /scratch/rx32940/CLARK/output/result
 
@@ -36,7 +36,7 @@ data_path="/scratch/rx32940/Metagenomic_taxon_profile/Data/01.Data/hostclean"
 # analyze result from regular clark
 for file in /scratch/rx32940/CLARK/output/*; do 
     sample_csv=$(basename "$file" ".txt.csv")
-    $path/CLARK/CLARKSCV1.2.6.1/estimate_abundance.sh -F /scratch/rx32940/CLARK/output/$sample_csv.txt.csv -D $path/CLARK/DB > /scratch/rx32940/CLARK/output/${sample_csv}_abundance.txt
+    $path/CLARK/CLARKSCV1.2.6.1/estimate_abundance.sh -F /scratch/rx32940/CLARK/output/bacteria_phylum/$sample_csv.txt.csv -D $path/CLARK/DB > /scratch/rx32940/CLARK/output/bacteria_phylum/${sample_csv}_abundance.txt
 done
 
 # databases of discriminative spaced 31-mers
