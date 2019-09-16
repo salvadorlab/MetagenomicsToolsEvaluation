3fb1e713d058f8ce9a13026a120a000bfa242e45
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-09-16 19:43:23 -0400

    downstream analysis

--- CLARK_genus_visual.R
+++ CLARK_genus_visual.R
@@ -1,16 +1,16 @@
 library(xlsx)
 library(dplyr)
 
-setwd("/Users/rx32940/Dropbox/5. Rachel's projects/Metagenomic_Analysis/CLARK:CLARK(s)/results_phylum")
+setwd("/Users/rx32940/Dropbox/5. Rachel's projects/Metagenomic_Analysis/CLARK:CLARK(s)/results")
 
 all_files <- list.files(".")
 
 for (file in all_files){
-  current <- read.xlsx2(file,1) %>% select(1,2,3,11) 
+  current <- read.xlsx2(file,1) %>% select(1,3,4,5,6,7,9) 
   sample_name <- unlist(strsplit(file,"_."))[1] #extract sample name
-  colnames(current)[4] <- sample_name # rename proportion with sample name
+  colnames(current)[7] <- sample_name # rename proportion with sample name
   # if else feed the first sample R22.K into the overall table
-  current[,4] <- as.numeric(as.character(current[,4])) * (1/100)
+  current[,7] <- as.numeric(as.character(current[,7])) 
   if (sample_name != "R22.K"){
     all_samples <- full_join(all_samples, current, by = NULL) # combine samples into one table
   }
@@ -21,6 +21,6 @@ for (file in all_files){
 
 all_samples <- arrange(all_samples,Lineage) 
 
-write.csv(all_samples, "../all_samples_phylum.csv")
+write.csv(all_samples, "../all_samples_absolute_lineage.csv")
 
 # all_samples.csv was imported into excel file, formatted and saved as all_samples_CLARK.xlsx
