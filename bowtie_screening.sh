290229a85bfea94a55536712d621ce076e9fcf66
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-09-25 09:29:08 -0400

    fix alignment command

--- bowtie_screening.sh
+++ bowtie_screening.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#PBS -q batch                                                          
-#PBS -N species_classify                                           
-#PBS -l nodes=1:ppn=2 -l mem=30gb                                        
-#PBS -l walltime=30:00:00                                                
+#PBS -q highmem                                                          
+#PBS -N hostclean                                           
+#PBS -l nodes=1:ppn=2 -l mem=100gb                                        
+#PBS -l walltime=100:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
 #PBS -o /scratch/rx32940/Metagenomic_taxon_profile                       
@@ -13,10 +13,14 @@
 module load Bowtie2/2.3.4.1-foss-2016b
 
 DATA="/scratch/rx32940/Metagenomic_taxon_profile/Data"
-bowtie2-build $DATA/refSeq/rn6.fa $DATA/host_DB
+bowtie2-build $DATA/refSeq/rn6.fa $DATA/host_DB # build index for reference genome, in .bt2 format
+echo "bowtie-build done"
 
 for file in $DATA/rawProcessed/*; do
     SAMPLE=$(basename "$file")
-    bowtie2 -f --reorder -x host_DB -U $file --un $DATA/hostCleaned/$SAMPLE.fna 
+    
+    bowtie2 -f --reorder -x $DATA/host_DB -U $file --un $DATA/hostCleaned/  # -x ask for the basename of the index files
+
+    echo "$SAMPLE done"
 done
 
