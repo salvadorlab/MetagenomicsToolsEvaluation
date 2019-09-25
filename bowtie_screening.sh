41628eb490c72da9285e548dcbf48b3765c8f2ae
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-09-25 13:46:09 -0400

    separate dir for each sample

--- bowtie_screening.sh
+++ bowtie_screening.sh
@@ -13,13 +13,13 @@
 module load Bowtie2/2.3.4.1-foss-2016b
 
 DATA="/scratch/rx32940/Metagenomic_taxon_profile/Data"
-bowtie2-build $DATA/refSeq/rn6.fa $DATA/host_DB # build index for reference genome, in .bt2 format
-echo "bowtie-build done"
+# bowtie2-build $DATA/refSeq/rn6.fa $DATA/host_DB # build index for reference genome, in .bt2 format
+# echo "bowtie-build done"
 
 for file in $DATA/rawProcessed/*; do
     SAMPLE=$(basename "$file")
-    
-    bowtie2 -f --reorder -x $DATA/host_DB -U $file --un $DATA/hostCleaned/  # -x ask for the basename of the index files
+    mkdir $SAMPLE
+    bowtie2 -f --reorder -x $DATA/host_DB -U $file --un $DATA/hostCleaned/$SAMPLE/  # -x ask for the basename of the index files
 
     echo "$SAMPLE done"
 done
