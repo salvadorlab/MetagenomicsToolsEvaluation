4ec4b98162c62281a8d413b4fb78b85d27f54763
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-09-25 17:22:05 -0400

    commit

--- bowtie_screening.sh
+++ bowtie_screening.sh
@@ -18,7 +18,7 @@ DATA="/scratch/rx32940/Metagenomic_taxon_profile/Data"
 
 for file in $DATA/rawProcessed/*; do
     SAMPLE=$(basename "$file" ".fna")
-    mkdir $SAMPLE
+    mkdir $DATA/hostCleaned/$SAMPLE
 
     echo "$SAMPLE start"
     bowtie2 -f --reorder -x $DATA/host_DB -U $file --un $DATA/hostCleaned/$SAMPLE/$SAMPLE.fna  # -x ask for the basename of the index files
