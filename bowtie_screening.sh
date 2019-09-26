5b9cea0c1d433a2c66a2c4ee7c736c595c814499
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-09-25 20:05:18 -0400

    more resources hostclean

--- bowtie_screening.sh
+++ bowtie_screening.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q highmem_q                                                        
 #PBS -N hostclean                                           
-#PBS -l nodes=1:ppn=2 -l mem=100gb                                        
+#PBS -l nodes=1:ppn=4 -l mem=200gb                                        
 #PBS -l walltime=100:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
