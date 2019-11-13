0ba04b0b53614303c4cd59c477a297206b296926
rx32940 <rx32940@uga.edu>
2019-11-12 23:03:08 -0500

    genus custom clarks

--- run_clark.sh
+++ run_clark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -q highmem_q                                                          
-#PBS -N custom_phylum_abundance                                          
+#PBS -N custom_genus_abundance                                          
 #PBS -l nodes=1:ppn=12 -l mem=500gb                                        
 #PBS -l walltime=200:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
@@ -13,8 +13,8 @@ path="/scratch/rx32940"
 data_path="/scratch/rx32940/Metagenomic_taxon_profile/Data/01.Data/hostclean"
 
 # set up the database
-# $path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB custom --genus
-$path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB custom --phylum
+$path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB custom --genus
+# $path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB custom --phylum
 # $path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB custom --species
 # $path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB bacteria viruses --species
 # $path/CLARK/CLARKSCV1.2.6.1/set_targets.sh $path/CLARK/DB bacteria viruses --phylum
@@ -31,14 +31,14 @@ echo "set target done"
 #     $path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -P $data_path/$sample/${sample}_1_kneaddata_paired_1.fastq $data_path/$sample/${sample}_1_kneaddata_paired_2.fastq -R /scratch/rx32940/CLARK/output/custom/genus/regular/$sample.txt
 # done
 
-$path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -n 12 -P $path/CLARK/CLARKSCV1.2.6.1/samples.R.txt $path/CLARK/CLARKSCV1.2.6.1/samples.L.txt -R $path/CLARK/CLARKSCV1.2.6.1/results_P_regular.txt
+$path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -n 12 -P $path/CLARK/CLARKSCV1.2.6.1/samples.R.txt $path/CLARK/CLARKSCV1.2.6.1/samples.L.txt -R $path/CLARK/CLARKSCV1.2.6.1/results_G_regular.txt
 
 echo "classify_metagenome done"
 
 #analyze result from regular clark
-for file in /scratch/rx32940/CLARK/output/custom/phylum/regular/*; do 
+for file in /scratch/rx32940/CLARK/output/custom/genus/regular/*; do 
    sample_csv=$(basename "$file" ".csv")
-  $path/CLARK/CLARKSCV1.2.6.1/estimate_abundance.sh -F /scratch/rx32940/CLARK/output/custom/phylum/regular/$sample_csv.csv -D $path/CLARK/DB > /scratch/rx32940/CLARK/output/abundance/phylum/${sample_csv}_abundance.txt
+  $path/CLARK/CLARKSCV1.2.6.1/estimate_abundance.sh -F /scratch/rx32940/CLARK/output/custom/genus/regular/$sample_csv.csv -D $path/CLARK/DB > /scratch/rx32940/CLARK/output/abundance/genus/${sample_csv}_abundance.txt
 done
 
 echo "regular abundance estimation done"
@@ -54,14 +54,14 @@ echo "spaced database built"
 #     $path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -n 12 -P $data_path/$sample/${sample}_1_kneaddata_paired_1.fastq $data_path/$sample/${sample}_1_kneaddata_paired_2.fastq -R /scratch/rx32940/CLARK/output/custom/genus/spaced/${sample}_spaced --spaced
 # done
 
-$path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -n 12 -P $path/CLARK/CLARKSCV1.2.6.1/samples.R.txt $path/CLARK/CLARKSCV1.2.6.1/samples.L.txt -R $path/CLARK/CLARKSCV1.2.6.1/results_P_spaced.txt --spaced
+$path/CLARK/CLARKSCV1.2.6.1/classify_metagenome.sh -n 12 -P $path/CLARK/CLARKSCV1.2.6.1/samples.R.txt $path/CLARK/CLARKSCV1.2.6.1/samples.L.txt -R $path/CLARK/CLARKSCV1.2.6.1/results_G_spaced.txt --spaced
 
 echo "spaced classification done"
 
 #analyze result from spaced clark
-for file in /scratch/rx32940/CLARK/output/custom/phylum/spaced/*; do 
+for file in /scratch/rx32940/CLARK/output/custom/genus/spaced/*; do 
    sample_csv=$(basename "$file" "_spaced.csv")
-  $path/CLARK/CLARKSCV1.2.6.1/estimate_abundance.sh -F /scratch/rx32940/CLARK/output/custom/phylum/spaced/${sample_csv}_spaced.csv -D $path/CLARK/DB > /scratch/rx32940/CLARK/output/spaced_abundance/phylum/${sample_csv}_spaced_abundance.txt
+  $path/CLARK/CLARKSCV1.2.6.1/estimate_abundance.sh -F /scratch/rx32940/CLARK/output/custom/genus/spaced/${sample_csv}_spaced.csv -D $path/CLARK/DB > /scratch/rx32940/CLARK/output/spaced_abundance/genus/${sample_csv}_spaced_abundance.txt
 done
 
 echo "spaced abundance estimation done"
