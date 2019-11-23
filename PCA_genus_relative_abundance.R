02728fca886c0bb74a9648a05bc565c3193c9ea5
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-11-23 15:33:08 -0500

    Update PCA_genus_relative_abundance.R

--- PCA_genus_relative_abundance.R
+++ PCA_genus_relative_abundance.R
@@ -1,6 +1,9 @@
 library(xlsx)
 library(devtools)
-library(factoextra) #for pca visualization
+library(factoextra)
+library(ggfortify)
+library(tidyverse)#for pca visualization
+library(ggplot2)
 
 # this pca analysis is done with relative abundance of bacteria and viruses composition in the samples
 # the report from the company is based only on bacterial pyhlum relative abundance (viruses do not classify by phylum)
@@ -8,22 +11,37 @@ library(factoextra) #for pca visualization
 #tutorial for pca
 # http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/118-principal-component-analysis-in-r-prcomp-vs-princomp/#prcomp-and-princomp-functions
 
+setwd("/Users/rx32940/Dropbox/5.Rachel-projects/Metagenomic_Analysis/Clark-s/genus")
 
-
-data_genus <- read_csv("/Users/rx32940/Dropbox/5. Rachel's projects/Metagenomic_Analysis/CLARK:CLARK(s)/all_samples_phylum.csv")
+data_genus <- read.csv("./classifiedOnly_count_clarks_genus_custom.csv",sep = ",",header=TRUE)
 rownames(data_genus) <- data_genus$Name
-data_genus <- select(data_genus, -Name)
+data_genus <- select(data_genus, -Name) 
+#data_genus <-  select(data_genus, -X) 
+data_genus <- data_genus[-c(1:2),]
 
 
 data2_genus <- data.frame(lapply(data_genus, function(x) as.numeric(as.character(x))))
 data2_genus <-t(data2_genus)
-pca_genus <- prcomp(data2_genus,center=TRUE,scale. = TRUE)
+data2_genus[is.na(data2_genus)] <- 0
+data2_genus <- as.data.frame(data2_genus)
+data2_genus$Tissues <- as.list(rep(c("Kidney","Lung","Spleen"),4))
+pca_genus <- prcomp(data2_genus[,-c(438)],center=TRUE,scale. = TRUE)
 summary(pca_genus)
+data2_genus$Tissues <- factor(unlist(data2_genus$Tissues))
+class(data2_genus$Tissues) <- "factor"
+plot <- autoplot(pca_genus,data = data2_genus,colour ="Tissues",label=TRUE) 
+
+plot <- plot + ggtitle("PCA Analysis for Clark-s in the Genus Level") +
+  theme_bw()
+plot
+ggsave("./pca_G_plot_clarks.png",plot)
 
 eigenvalues_genus <- fviz_eig(pca_genus)
 eigenvalues_genus
-png("/Users/rx32940/Dropbox/5. Rachel's projects/Metagenomic_Analysis/CLARK:CLARK(s)/pca_P_plot.png")
-fviz_pca_ind(pca_genus,col.var="cos2")
+pca <- fviz_pca_ind(pca_genus,col.var="cos2")
+
+png("./pca_P_plot_Kraken2.png")
+
 dev.off()
 
 ##################################################
