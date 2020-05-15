fb09295cf8a654ade0c21692818a2cb3d746a323
rx32940 <rx32940@uga.edu>
2020-05-15 16:10:14 -0400

    bowtie2 build database for Rrattus

--- kraken2_pipeline.sh
+++ kraken2_pipeline.sh
@@ -18,7 +18,7 @@ cd $PBS_O_WORKDIR
 
 seq_path="/scratch/rx32940/kraken2_052020/kneaddata"
 # build database for host reference (refseq for R.rattus and R.norvegicus_Rnor_6.0) for kneaddate cleaning
-bowtie2-build $seq_path/reference_seq/GCF_000001895.5_Rnor_6.0_genomic.fna $seq_path/ref_db/
-
+# bowtie2-build $seq_path/reference_seq/GCF_000001895.5_Rnor_6.0_genomic.fna $seq_path/ref_db/
+bowtie2-build $seq_path/reference_seq/GCF_011064425.1_Rrattus_CSIRO_v1_genomic.fna $seq_path/ref_db/
 # host clean with KneadData (downloaded to sapelo2 home dir: pip install --user kneaddate)
 # kneaddata --trimmomatic /usr/local/apps/eb/Trimmomatic/0.33-Java-1.8.0_144
