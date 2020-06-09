cdf71a0a5a9217416a740d81541f8dcbc8e98a1d
rx32940 <rx32940@uga.edu>
2020-06-09 18:42:44 -0400

    hostclean seq

--- kraken2_pipeline.sh
+++ kraken2_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -q batch                                                            
-#PBS -N bowtie2_rnor                                            
-#PBS -l nodes=1:ppn=12 -l mem=100gb                                        
+#PBS -q bahl_salv_q                                                            
+#PBS -N submit_hostclean                                           
+#PBS -l nodes=1:ppn=2 -l mem=10gb                                        
 #PBS -l walltime=300:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
@@ -9,20 +9,62 @@
 #PBS -e /scratch/rx32940/                     
 #PBS -j oe   
 
-module load Bowtie2/2.3.5.1-foss-2018a
-module load Trimmomatic/0.36-Java-1.8.0_144
-module load FastQC/0.11.8-Java-1.8.0_144 
-module load SAMtools/1.10-GCC-8.2.0-2.31.1
-
 cd $PBS_O_WORKDIR
 
-seq_path="/scratch/rx32940/kraken2_052020/kneaddata"
+seq_path="/scratch/rx32940/kraken2_052020"
+DATABASE="/scratch/rx32940/kraken2_052020/kneaddata/ref_db"
 # build database for host reference (refseq for R.rattus and R.norvegicus_Rnor_6.0) for kneaddate cleaning
-bowtie2-build $seq_path/reference_seq/GCF_000001895.5_Rnor_6.0_genomic.fna $seq_path/ref_db/Rnor_6.0
-# bowtie2-build $seq_path/reference_seq/GCF_011064425.1_Rrattus_CSIRO_v1_genomic.fna $seq_path/ref_db/Rrattus
-# host clean with KneadData (downloaded to sapelo2 home dir: pip install --user kneaddate)
-# kneaddata --trimmomatic /usr/local/apps/eb/Trimmomatic/0.33-Java-1.8.0_144
+# bowtie2-build $seq_path/kneaddata/reference_seq/GCF_000001895.5_Rnor_6.0_genomic.fna $seq_path/kneaddata/ref_db/Rnor_6.0
+# bowtie2-build $seq_path/kneaddata/reference_seq/GCF_011064425.1_Rrattus_CSIRO_v1_genomic.fna $seq_path/kneaddata/ref_db/Rrattus
 
+# host clean with KneadData (downloaded to sapelo2 home dir: pip install --user kneaddate)
+for sample in $seq_path/Data/rawdata/*;
+do
+    (
+        sample_id="$(basename "$sample" | awk -F"." '{print $1}')"
+        tissue_id="$(basename "$sample")"
+        species="species"
+
+        if [ "$sample_id" == "R28" ] ; 
+        then 
+            species="Rrattus"
+        else 
+            species="Rnor_6.0"
+            fi
+
+        sapelo2_header="#PBS -q bahl_salv_q\n#PBS -N hostclean_$tissue_id\n
+        #PBS -l nodes=1:ppn=12 -l mem=20gb\n
+        #PBS -l walltime=100:00:00\n
+        #PBS -M rx32940@uga.edu\n                                                  
+        #PBS -m abe\n                                                            
+        #PBS -o /scratch/rx32940\n                      
+        #PBS -e /scratch/rx32940\n                        
+        #PBS -j oe\n
+        "
+        echo $sample
+        echo $species
+
+        echo -e $sapelo2_header > $seq_path/qsub_kneaddata.sh 
+
+        modules="module load Bowtie2/2.3.5.1-foss-2018a\n
+        module load Trimmomatic/0.36-Java-1.8.0_144\n
+        module load FastQC/0.11.8-Java-1.8.0_144\n 
+        module load SAMtools/1.10-GCC-8.2.0-2.31.1"
+
+        echo -e $modules >> $seq_path/qsub_kneaddata.sh 
+        
+        echo "kneaddata -t 12 -v --trimmomatic /usr/local/apps/eb/Trimmomatic/0.33-Java-1.8.0_144 \
+        --input $sample/*_1.fq.gz --input $sample/*_1.fq.gz \
+        -db $DATABASE/$species --output $seq_path/kneaddata/hostclean_self" >> $seq_path/qsub_kneaddata.sh
+    
+        qsub $seq_path/qsub_kneaddata.sh
+
+        echo "submit $sample"
+    ) &
+
+    wait 
+    echo "waiting"
+done
 
 # module load Kraken2/2.0.7-beta-foss-2018a-Perl-5.26.1
 # DBNAME="/scratch/rx32940/kraken2_052020/kraken2/kraken2_db"
