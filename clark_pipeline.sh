e7f0241c949871a7c7c743714b6206d9151c085c
rx32940 <rx32940@uga.edu>
2020-06-15 21:14:57 -0400

    classify seq with clark (building discriminative db

--- clark_pipeline.sh
+++ clark_pipeline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#PBS -q bahl_salv_q                                                            
-#PBS -N clark_virus                                        
-#PBS -l nodes=1:ppn=1 -l mem=100gb                                        
-#PBS -l walltime=200:00:00                                                
+#PBS -q highmem_q                                                            
+#PBS -N clark_classify                                        
+#PBS -l nodes=1:ppn=15 -l mem=300gb                                        
+#PBS -l walltime=150:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
 #PBS -o /scratch/rx32940/
@@ -11,21 +11,54 @@
 
 path="/scratch/rx32940/clark_0613"
 DB="/scratch/rx32940/clark_0613/database"
+seq_path="/scratch/rx32940/clark_0613/hostclean_seq"
 
 ###################################################################
 #
-# Selecting the database(s)
+# Building the database(s)
 # select among 'bacteria', 'viruses', 'plasmid', 'plastid', 'protozoa', 'fungi', 'human' and/or 'custom'
 # genomes from NCBI/RefSeq will be downloaded if they are not present in $DB
-#
+# 
+# *** Custom database built with cp Bacteria, Human, Viruses databases into Custom
+#     Wget Univec_core from NCBI's ftp: https://ftp.ncbi.nlm.nih.gov/pub/UniVec/UniVec_Core
 ###################################################################
 
 # download databases provided by clark that matches kraken2's standard library 
-#(clark didn't provide an UniVec database)
-# added univec_core database from https://ftp.ncbi.nlm.nih.gov/pub/UniVec/UniVec_Core
 # each category submit separate for speed 
 
 # $path/CLARKSCV1.2.6.1/set_targets.sh $DB/standard bacteria 
-$path/CLARKSCV1.2.6.1/set_targets.sh $DB/standard viruses 
+# $path/CLARKSCV1.2.6.1/set_targets.sh $DB/standard viruses 
 # $path/CLARKSCV1.2.6.1/set_targets.sh $DB/standard human 
- 
+
+# build custom database after added Univec_core
+# $path/CLARKSCV1.2.6.1/set_targets.sh $DB/standard custom 
+
+###################################################################
+#
+# Setting Taxonomy rank
+# The default taxonomy rank is species
+#
+###################################################################
+
+$path/CLARKSCV1.2.6.1/set_targets.sh $DB/standard custom bacteria viruses human --phylum
+# # $path/CLARKSCV1.2.6.1/set_targets.sh $DB/standard Custom --genus
+
+# classify each sequence
+for file in $seq_path/*; do
+    sample=$(basename "$file" "_1_kneaddata_unmatched_1.fastq")
+    $path/CLARKSCV1.2.6.1/classify_metagenome.sh -n 24 -O $seq_path/${sample}_1_kneaddata_unmatched_1.fastq -R $path/output_phylum/$sample
+done
+
+echo "classify_metagenome done"
+
+#analyze result from clark
+# for file in /scratch/rx32940/CLARK/output/prebuilt/phylum/regular/*; do 
+#    sample_csv=$(basename "$file" ".csv")
+#   $path/CLARK/CLARKSCV1.2.6.1/estimate_abundance.sh -F /scratch/rx32940/CLARK/output/prebuilt/phylum/regular/$sample_csv.csv -D $path/CLARK/DB > /scratch/rx32940/CLARK/output/abundance/phylum/prebuilt/${sample_csv}_abundance.txt
+# done
+
+# echo "regular abundance estimation done"
+
+# build spaced database for clark-s 
+# cd $path/CLARKSCV1.2.6.1
+# ./buildSpacedDB.sh
