7b7d0370f512119a0020a6b58d5da16caf12fde4
rx32940 <rx32940@uga.edu>
2020-07-01 17:11:42 -0400

    clark pair end seq id sorted, reran, building clarks db

--- clark_pipeline.sh
+++ clark_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q highmem_q                                                            
-#PBS -N clark-spaced-db                                       
-#PBS -l nodes=1:ppn=1 -l mem=800gb                                        
+#PBS -N clark-classify                                       
+#PBS -l nodes=1:ppn=12 -l mem=200gb                                        
 #PBS -l walltime=300:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
@@ -36,6 +36,26 @@ seq_path="/scratch/rx32940/clark_0613/hostclean_seq"
 # reset custom db, added refseq for Rattus into custom db along with univec_core
 # no need to set_targets separately with custom db. straight set_target with rest of the db, default is species level
 # set targets is species level, provide full taxonomic lineage in the report. can do conversion in R to speed up the analysis
+
+
+##################################################################
+#
+# clark cannot process pair end reads with unmatching read id 
+# order of forward and backward reads not matching
+# used this tool to mach the order of the forward and reverse file:
+# fastq-pair: https://github.com/linsalrob/fastq-pair.git
+# no need to submit to sapelo2, run this part locally
+#
+###################################################################
+
+# cd /scratch/rx32940/clark_0613/hostclean_seq
+
+# cat /scratch/rx32940/kraken2_052020/kneaddata/metagenomic_samples.txt | \
+# while read sample; do
+# fastq_pair ${sample}_1_kneaddata_paired_1.fastq ${sample}_1_kneaddata_paired_2.fastq
+# done
+
+
 ###################################################################
 #
 # Setting Taxonomy rank
@@ -48,26 +68,26 @@ seq_path="/scratch/rx32940/clark_0613/hostclean_seq"
 $path/CLARKSCV1.2.6.1/set_targets.sh $DB/standard custom bacteria viruses human --species
 
 # use one sample to build the discriminative database
-$path/CLARKSCV1.2.6.1/classify_metagenome.sh -n 1 -P $seq_path/R22.K_1_kneaddata_paired_1.fastq $seq_path/R22.K_1_kneaddata_paired_2.fastq -R $path/output_species/R22.K
+#$path/CLARKSCV1.2.6.1/classify_metagenome.sh -n 1 -P $seq_path/${sample}_1_kneaddata_paired_1.fastq.paired.fq $seq_path/${sample}_1_kneaddata_paired_2.fastq.paired.fq -R $path/output_species/R22.K
 
 # classify each sequence
-# cat /scratch/rx32940/kraken2_052020/kneaddata/metagenomic_samples.txt | \
-# while read sample;
-# do
-#    #  sample=$(basename "$file" "_1_kneaddata_unmatched_1.fastq")
-#     $path/CLARKSCV1.2.6.1/classify_metagenome.sh -n 24 -P $seq_path/${sample}_1_kneaddata_paired_1.fastq $seq_path/${sample}_1_kneaddata_paired_2.fastq -R $path/output_species/$sample
-# done
+cat /scratch/rx32940/kraken2_052020/kneaddata/metagenomic_samples.txt | \
+while read sample;
+do
+   #  sample=$(basename "$file" "_1_kneaddata_unmatched_1.fastq")
+    $path/CLARKSCV1.2.6.1/classify_metagenome.sh -n 12 -P $seq_path/${sample}_1_kneaddata_paired_1.fastq.paired.fq $seq_path/${sample}_1_kneaddata_paired_2.fastq.paired.fq -R $path/output_species/$sample
+done
 
 # $path/CLARKSCV1.2.6.1/classify_metagenome.sh -n 12 -P $path/samples.R.txt $path/samples.L.txt -R $path/output_species/samples.results.txt
 
 # analyze result from clark
-# for file in /scratch/rx32940/clark_0613/output_species/*; do 
-#    sample_csv=$(basename "$file" ".csv")
-#    $path/CLARKSCV1.2.6.1/estimate_abundance.sh -F /scratch/rx32940/clark_0613/output_species/$sample_csv.csv -D $DB/standard > /scratch/rx32940/clark_0613/output_species/${sample_csv}_abundance.txt
-# done
+for file in /scratch/rx32940/clark_0613/output_species/*; do 
+   sample_csv=$(basename "$file" ".csv")
+   $path/CLARKSCV1.2.6.1/estimate_abundance.sh -F /scratch/rx32940/clark_0613/output_species/$sample_csv.csv -D $DB/standard > /scratch/rx32940/clark_0613/output_species/${sample_csv}_abundance.txt
+done
 
-# echo "regular abundance estimation done"
+echo "regular abundance estimation done"
 
-# build spaced database for clark-s 
-cd $path/CLARKSCV1.2.6.1
-./buildSpacedDB.sh
+# # build spaced database for clark-s 
+# cd $path/CLARKSCV1.2.6.1
+# ./buildSpacedDB.sh
