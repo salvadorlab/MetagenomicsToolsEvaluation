887245889ae30127a2d1f24b3fa2ded677b19c99
Rachel Xu <rx32940@uga.edu>
2021-03-13 07:18:06 -0500

    commit

--- .#clark_pipeline.sh
+++
@@ -1,1 +0,0 @@
-rx32940@sapelo2-sub5.ecompute.39140:1588182559
--- run_bracken.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-path='/scratch/rx32940/Metagenomic_taxon_profile'
-for file in $path/kraken_output/*kreport; do
-    sample=$(basename $file .kreport)
-    $path/bracken/Bracken-2.5/bracken -d $path/kraken/minikraken2_v1_8GB_201904_UPDATE -i $path/kraken_output/$sample.kreport -o $path/bracken/output/phylum/$sample -l P -t 10
-done
